13e036c2e6652469de2173a395b2c07cb1bf7206
623446617 <623446617@qq.com>
2020-12-26 20:50:03 +0800

    创建x-button-group.vue，并且验证子组件是否为x-button

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import xbutton from "./x-button";
 import xicon from "./x-icon";
-
+import xbuttongroup from './x-button-group';
 // 注册全局组件
 Vue.component('x-button', xbutton);
 Vue.component('x-icon', xicon);
+Vue.component('x-button-group', xbuttongroup)
 
 let app = new Vue({
     el: '#app',
---
+++ src/x-button-group.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="x-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "x-button-group",
+        mounted() {
+            this.validator('x-button');
+        },
+        methods: {
+            // 规定传入子组件类型
+            validator(cptname) {
+                this.$children.forEach((child, i) => {
+                    if (child.$options.name !== cptname) {
+                        throw new Error(`<${this.$options.name}/> 传入的第${i + 1}个子组件不为 <${cptname}/>`);
+                    }
+                });
+            }
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    .x-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+
+        > .x-button {
+            border-radius: 0;
+            margin-left: -1px;
+
+            &:first-child {
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+
+            &:last-child {
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+
+            &:hover {
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
