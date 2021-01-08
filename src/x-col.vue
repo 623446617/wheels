a96fceb1249343f2ead567fa3db84bc62d3f7d2d
623446617 <623446617@qq.com>
2021-01-08 22:22:40 +0800

    x-row添加gutter属性，x-col添加span、offset、gutter属性

--- src/x-col.vue
+++ src/x-col.vue
@@ -1,17 +1,31 @@
 <template>
-    <div class="x-col">
-        <slot></slot>
+    <div class="x-col"
+         :class="[span && `x-col-${span}`, offset && `x-offset-${offset}`]"
+         :style="gutter && `padding-right: ${gutter / 2}px; padding-left: ${gutter / 2}px;`">
+        <div style="border: 1px solid;">
+            <slot></slot>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: "x-col",
-        props: {
-            span: {
-                type: Number,
-                default: 1
+        data() {
+            return {
+                // 偏移
+                gutter: 0
             }
+        },
+        props: {
+            // 份数
+            span: {
+                type: [Number, String]
+            },
+            // 偏移
+            offset: {
+                type: [Number, String]
+            },
         }
     };
 </script>
@@ -19,13 +33,23 @@
 <style scoped lang="scss">
     .x-col {
         min-height: 5px;
+        /*宽度为 100%，d:f 默认不换行，则默认均等分x-row*/
+        width: 100%;
 
-        $prefix: x-col-;
         $start: 1;
         $end: 24;
+
+        $span: x-col-;
         @for $n from $start through $end {
-            &.#{$prefix}#{$n} {
+            &.#{$span}#{$n} {
                 width: ($n / 24) * 100%;
+            }
+        }
+
+        $offset: x-offset-;
+        @for $n from $start through $end {
+            &.#{$offset}#{$n} {
+                margin-left: ($n / 24) * 100%;
             }
         }
     }
--- src/x-row.vue
+++ src/x-row.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="x-row">
+    <div class="x-row" :style="gutter && `margin-right: -${gutter / 2}px; margin-left: -${gutter / 2}px;`">
         <slot></slot>
     </div>
 </template>
@@ -9,9 +9,25 @@
 
     export default {
         name: "x-row",
+        props: {
+            gutter: {
+                type: [Number, String]
+            }
+        },
         mixins: [childValidatorMixin],
         mounted() {
             this.validator('x-col');
+            this.setChildGutter();
+        },
+        methods: {
+            // 给子组件x-col设置gutter属性
+            setChildGutter() {
+                if (this.gutter) {
+                    this.$children.forEach(vm => {
+                        vm.gutter = this.gutter;
+                    });
+                }
+            }
         }
     };
 </script>
