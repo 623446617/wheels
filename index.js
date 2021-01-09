5acab46170862acb997bc37eb4e8e0065ab0a314
623446617 <623446617@qq.com>
2021-01-09 21:59:29 +0800

    npm打包的输出文件添加x-input、x-row、x-row

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import "./src/base.css";    // 基础样式
 import XButton from './src/x-button';
 import XButtonGroup from './src/x-button-group';
 import XIcon from './src/x-icon';
+import XInput from './src/x-input';
+import XRow from './src/x-row';
+import XCol from './src/x-col';
 
 // Vue插件
 const plugin = {
@@ -12,6 +15,9 @@ const plugin = {
         Vue.component('x-button', XButton);
         Vue.component('x-button-group', XButtonGroup);
         Vue.component('x-icon', XIcon);
+        Vue.component('x-input', XInput);
+        Vue.component('x-row', XRow);
+        Vue.component('x-col', XCol);
     }
 };
 
@@ -20,5 +26,8 @@ export default plugin;
 export {
     XButton,
     XButtonGroup,
-    XIcon
+    XIcon,
+    XInput,
+    XRow,
+    XCol
 };
