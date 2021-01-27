85b2a3672e14cde93899f27bb7d9f7054e3efd1d
623446617 <623446617@qq.com>
2021-01-27 22:28:36 +0800

    更新x-popover样式，添加layout属性，包括竖直与水平方向，及箭头朝向与位置

--- src/x-popover.vue
+++ src/x-popover.vue
@@ -1,16 +1,15 @@
 <template>
-    <div class="x-popover-wrapper" ref="popover-wrapper" @click.stop="click">
+    <div class="x-popover-wrapper" ref="wrapper" @click.stop="click">
         <div ref="popover"
              @click.stop
              class="x-popover"
+             :class="layout"
              :style="{display: visible? 'block': 'none', ...position}">
             <slot name="content"></slot>
 
             <div class="x-popover-arrow" :class="{[`x-popover-arrow-${arrowLayout}`]: !!arrowLayout}"></div>
         </div>
         <slot></slot>
-        <div class="test1"></div>
-        <div class="test2"></div>
     </div>
 </template>
 
@@ -59,12 +58,14 @@
                 document.addEventListener('scroll', this.popoverInWindowPosition);
                 window.addEventListener('resize', this.popoverInWindowPosition);
             },
+
             removeListener() {
                 // 移出document上的监听
                 document.removeEventListener('scroll', this.popoverInWindowPosition);
                 window.removeEventListener('resize', this.popoverInWindowPosition);
                 document.removeEventListener('click', this.documentEventHandler);
             },
+
             click() {
                 // 显示当前的popover，隐藏其他的popover
                 this.$XWHEELEVENTBUS.$emit('XWHEELPOPOVERDISMISS', this);
@@ -84,6 +85,7 @@
                     this.clearPopoverPosition();
                 }
             },
+
             // 将popover添加到body上
             addPopoverToBody() {
                 if (!this.popoverRef) {
@@ -91,58 +93,112 @@
                     document.documentElement.appendChild(this.popoverRef);
                 }
             },
+
             // 点击document时，隐藏popover
             documentEventHandler() {
                 this.visible = false;
                 document.removeEventListener('click', this.documentEventHandler);
                 console.log('documentEventHandler');
             },
-            // 计算popover在窗口的位置
-            popoverInWindowPosition() {
-                // 获取当前点击元素位置信息
-                let rect = this.$refs['popover-wrapper'].getBoundingClientRect();
 
-                console.log(rect);
-                let verticalMiddle = rect.top + rect.height / 2;
-
-                let documentHeight = document.documentElement.clientHeight,
-                    documentWidth = document.documentElement.clientWidth;
-
+            // 竖直方向位置
+            verticalPosition(wrapperRect, popover, documentHeight, documentWidth, margin = 15) {
                 let position = {};
                 let arrowLayout = '';
 
-                // 判断popover上下位置与箭头位置
-                if (verticalMiddle >= documentHeight / 2) {
-                    console.log('popover在上');
-                    position.bottom = `${documentHeight - rect.top + 15}px`;
+                // 点击元素的（竖直）中间位置
+                let middle = wrapperRect.top + wrapperRect.height / 2;
 
-                    console.log('arrow朝下');
+                /*
+                *
+                * 判断popover位置与箭头朝向
+                *
+                * */
+                // 点击元素在屏幕上半区
+                if (middle >= documentHeight / 2) {
+                    // popover在上，arrow朝下
+                    position.bottom = `${documentHeight - wrapperRect.top + margin}px`;
                     arrowLayout = 'bottom';
-                } else {
-                    console.log('popover在下');
-                    position.top = `${rect.bottom + 15}px`;
-                    console.log('arrow朝上');
+                }
+                // 点击元素在屏幕下半区
+                else {
+                    // popover在下，arrow朝上
+                    position.top = `${wrapperRect.bottom + margin}px`;
                     arrowLayout = 'top';
                 }
 
-                const popover = this.$refs['popover'];
                 const width = popover.clientWidth;
-                console.log(width)
 
                 // 箭头居左、中、右
-                if (rect.left < width) {
-                    position.left = `${rect.left}px`;
+                if (wrapperRect.left < width) {
+                    position.left = `${wrapperRect.left}px`;
                     arrowLayout += '-left';
-                } else if (documentWidth - rect.right < width) {
-                    position.right = `${documentWidth - rect.right}px`;
+                } else if (documentWidth - wrapperRect.right < width) {
+                    position.right = `${documentWidth - wrapperRect.right}px`;
                     arrowLayout += '-right';
                 } else {
                     arrowLayout += '-center';
-                    position.left = `${rect.left + rect.width / 2 - width / 2}px`;
+                    position.left = `${wrapperRect.left + wrapperRect.width / 2 - width / 2}px`;
                 }
 
-                this.arrowLayout = arrowLayout;
+                return {position, arrowLayout};
+            },
 
+            // 水平方向位置
+            horizontalPosition(wrapperRect, popover, documentHeight, documentWidth, margin = 15) {
+                let position = {};
+                let arrowLayout = '';
+
+                // 点击元素的（水平）中间位置
+                let center = wrapperRect.left + wrapperRect.width / 2;
+
+                /*
+                *
+                * 判断popover位置与箭头朝向
+                *
+                * */
+                // 点击元素在屏幕右半区
+                if (center >= documentWidth / 2) {
+                    // popover在左，arrow朝右
+                    position.right = `${documentWidth - wrapperRect.left + margin}px`;
+                    arrowLayout = 'right';
+                }
+                // 点击元素在屏幕左半区
+                else {
+                    // popover在右，arrow朝左
+                    position.left = `${wrapperRect.right + margin}px`;
+                    arrowLayout = 'left';
+                }
+
+                // 箭头居上、中、下
+                if (wrapperRect.top <= popover.clientHeight / 2) {
+                    position.top = `${wrapperRect.top}px`;
+                    arrowLayout += '-top';
+                } else if ((documentHeight - wrapperRect.bottom) <= popover.clientHeight / 2) {
+                    position.bottom = `${documentHeight - wrapperRect.bottom}px`;
+                    arrowLayout += '-bottom';
+                } else {
+                    position.top = `${wrapperRect.top + wrapperRect.height / 2 - popover.clientHeight / 2}px`;
+                    arrowLayout += '-middle';
+                }
+
+                return {position, arrowLayout};
+            },
+
+            // 计算popover在窗口的位置
+            popoverInWindowPosition() {
+                // 获取当前点击元素位置信息
+                const wrapperRect = this.$refs['wrapper'].getBoundingClientRect();
+                const popover = this.$refs['popover'];
+
+                // 获取窗体宽高
+                const documentHeight = document.documentElement.clientHeight,
+                    documentWidth = document.documentElement.clientWidth;
+
+                // 获取箭头位置与popover位置
+                const {arrowLayout, position} = this[`${this.layout}Position`](wrapperRect, popover, documentHeight, documentWidth);
+
+                this.arrowLayout = arrowLayout;
                 this.setPopoverPosition(position);
             },
             setPopoverPosition(position) {
@@ -163,38 +219,23 @@
 </script>
 
 <style scoped lang="scss">
-    .test1 {
-        position: fixed;
-        top: 50%;
-        left: 0;
-        width: 100%;
-        height: 1px;
-        background-color: red;
-    }
-    .test2 {
-        position: fixed;
-        top: 0;
-        left: 50%;
-        width: 1px;
-        height: 100%;
-        background-color: red;
-    }
-
     .x-popover-wrapper {
         display: inline-block;
         vertical-align: middle;
     }
 
+    $border-color: rgba(0, 0, 0, .2);
     .x-popover {
+        color: var(--color);
+        font-size: var(--font-size);
         max-width: 200px;
         min-width: 50px;
         word-break: break-all;
 
-        border: 1px solid;
         border-radius: 5px;
         padding: 5px 10px;
         background-color: #ffffff;
-        box-shadow: 0 2px 5px 0 rgba(black, 0.1);
+        box-shadow: 0 1px 6px $border-color;
 
         $arrow-l: 10px;
 
@@ -204,7 +245,7 @@
             position: absolute;
             transform: rotate(45deg);
             background-color: #ffffff;
-            border: 1px solid;
+            border: 1px solid hsla(0, 0%, 85%, .5);
 
             &::after {
                 width: $arrow-l;
@@ -215,43 +256,86 @@
             }
         }
 
+        /* 竖直 */
+        &.vertical {
+            .x-popover-arrow-top-center,
+            .x-popover-arrow-top-left,
+            .x-popover-arrow-top-right {
+                bottom: calc(100% - 4px);
 
-        .x-popover-arrow-top-center,
-        .x-popover-arrow-top-left,
-        .x-popover-arrow-top-right {
-            bottom: calc(100% - 4px);
+                &::after {
+                    top: 0;
+                    left: 0;
+                }
+            }
 
-            &::after {
-                top: 0;
-                left: 0;
+            .x-popover-arrow-bottom-center,
+            .x-popover-arrow-bottom-left,
+            .x-popover-arrow-bottom-right {
+                top: calc(100% - 4px);
+
+                &::after {
+                    top: -2px;
+                    left: -2px;
+                }
+            }
+
+            .x-popover-arrow-top-center,
+            .x-popover-arrow-bottom-center {
+                left: calc((100% - #{$arrow-l}) / 2);
+            }
+
+            .x-popover-arrow-top-left,
+            .x-popover-arrow-bottom-left {
+                left: 10%;
+            }
+
+            .x-popover-arrow-top-right,
+            .x-popover-arrow-bottom-right {
+                right: 10%;
             }
         }
 
-        .x-popover-arrow-bottom-center,
-        .x-popover-arrow-top-center {
-            left: calc((100% - #{$arrow-l}) / 2);
-        }
+        /* 水平 */
+        &.horizontal {
+            .x-popover-arrow-left-top,
+            .x-popover-arrow-left-middle,
+            .x-popover-arrow-left-bottom {
+                right: calc(100% - 4px);
 
-        .x-popover-arrow-bottom-left,
-        .x-popover-arrow-top-left {
-            left: 10%;
-        }
+                &::after {
+                    top: -2px;
+                    left: 0;
+                }
+            }
 
-        .x-popover-arrow-bottom-right,
-        .x-popover-arrow-top-right {
-            right: 10%;
-        }
+            .x-popover-arrow-right-top,
+            .x-popover-arrow-right-middle,
+            .x-popover-arrow-right-bottom {
+                left: calc(100% - 4px);
 
-        .x-popover-arrow-bottom-center,
-        .x-popover-arrow-bottom-left,
-        .x-popover-arrow-bottom-right {
-            top: calc(100% - 4px);
+                &::after {
+                    top: 0;
+                    left: -2px;
+                }
+            }
 
-            &::after {
-                top: -2px;
-                left: -2px;
+            .x-popover-arrow-left-top,
+            .x-popover-arrow-right-top {
+                top: 5%;
+            }
+
+            .x-popover-arrow-left-bottom,
+            .x-popover-arrow-right-bottom {
+                bottom: 5%;
+            }
+
+            .x-popover-arrow-left-middle,
+            .x-popover-arrow-right-middle {
+                top: calc((100% - #{$arrow-l}) / 2);
             }
         }
+
 
         /*.x-popover-arrow-left {
             bottom: calc(100% - 4px);
