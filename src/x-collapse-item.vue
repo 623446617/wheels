0fc97c172f68ec6084647272b7858cdfd3c8c628
623446617 <623446617@qq.com>
2021-02-04 22:08:46 +0800

    x-collapse-item样式完成，x-collapse添加mode属性（single：单个展开，multiple：多个展开）

--- src/x-collapse-item.vue
+++ src/x-collapse-item.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="x-collapse-item-wrapper" :class="{active: active}" >
-        <h4 class="x-collapse-item-title" @click="click">{{title}}</h4>
-        <div class="x-collapse-item-content" ref="cc">
+    <div class="x-collapse-item-wrapper" :class="{active: active}">
+        <h4 class="x-collapse-item-title" @click="click">
+            <x-icon name="right" class="x-collapse-item-arrow"></x-icon>
+            {{title}}
+        </h4>
+        <div class="x-collapse-item-content" ref="content">
             <slot></slot>
         </div>
     </div>
@@ -19,34 +22,94 @@
                 height: 0
             }
         },
+        watch: {
+          active(val) {
+              if (val) {
+                  this.$refs.content.style.height = `${this.height}px`;
+              } else {
+                  this.$refs.content.style.height = '0px';
+              }
+          }
+        },
         methods: {
             click() {
-                this.active = !this.active;
-                if (this.active) {
-                    this.$refs.cc.style.height = `${this.height}px`;
-                } else {
-                    this.$refs.cc.style.height = '0px';
-                }
+                this.$XWHEELEVENTBUS.$emit('XCOLLAPSEITEM-ACTIVE', this);
             }
         },
         mounted() {
-            this.height = this.$refs.cc.clientHeight;
-            this.$refs.cc.style.height = '0px';
+            this.height = this.$refs.content.clientHeight;
+            this.$refs.content.style.height = '0px';
         }
     };
 </script>
 
 <style scoped lang="scss">
     .x-collapse-item-wrapper {
+
+        /* 第一行的title没有上边框，但是有上圆角 */
+        &:first-child {
+            .x-collapse-item-title {
+                border-top-width: 0;
+                border-top-right-radius: var(--border-radius);
+                border-top-left-radius: var(--border-radius);
+            }
+        }
+
+        /* 最后一行的title有下圆角*/
+        &:last-child {
+            .x-collapse-item-title {
+                border-bottom-right-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+        }
+
+        &.active {
+            .x-collapse-item-title {
+                border-bottom-color: #dcdee2;
+
+                .x-collapse-item-arrow {
+                    transform: translateY(-50%) rotate(90deg);
+                }
+            }
+
+            &:last-child {
+                .x-collapse-item-title {
+                    border-radius: 0;
+                }
+
+                .x-collapse-item-content {
+                    border-bottom-right-radius: var(--border-radius);
+                    border-bottom-left-radius: var(--border-radius);
+                }
+            }
+        }
+
         .x-collapse-item-title {
-            background-color: #dcdee2;
+            padding: 5px;
+            padding-left: 30px;
+            position: relative;
+            border: 0 solid #dcdee2;
+            border-top-width: 1px;
+            border-bottom-width: 1px;
+            border-bottom-color: transparent;
+            transition: all 0.3s ease-in-out;
+
+            /* 箭头 */
+            .x-collapse-item-arrow {
+                width: 12px;
+                height: 12px;
+                position: absolute;
+                top: 50%;
+                left: 10px;
+                transform: translateY(-50%);
+                transition: transform 0.3s ease-in-out;
+            }
         }
 
         .x-collapse-item-content {
-            transition: height 0.5s;
+            transition: height 0.3s ease-in-out;
             overflow: hidden;
+            background-color: #ffffff;
         }
-
-
     }
 </style>
--- src/x-collapse.vue
+++ src/x-collapse.vue
@@ -6,12 +6,46 @@
 
 <script>
     export default {
-        name: "x-collapse"
+        name: "x-collapse",
+        props: {
+            // 单个展开，多个展开
+            mode: {
+                type: String,
+                default: 'single',
+                validator(val) {
+                    const bool = ['single', 'multiple'].indexOf(val) !== -1;
+
+                    if (!bool) {
+                        throw new Error(`props 'type' is not included in ['single', 'multiple'].`)
+                    }
+
+                    return bool;
+                }
+            }
+        },
+        mounted() {
+            this.$XWHEELEVENTBUS.$on('XCOLLAPSEITEM-ACTIVE', (vm) => {
+                if (this.mode === 'single') {
+                    this.$children.forEach(child => {
+                        if (child !== vm) {
+                            child.active = false;
+                        } else {
+                            vm.active = !child.active;
+                        }
+                    });
+                } else if (this.mode === 'multiple') {
+                    vm.active = !vm.active;
+                }
+            });
+        }
     };
 </script>
 
 <style scoped lang="scss">
     .x-collapse-wrapper {
-        border: 1px solid var(--border-color);
+        border: 1px solid #dcdee2;
+        border-radius: var(--border-radius);
+        background-color: #f7f7f7;
+
     }
 </style>
